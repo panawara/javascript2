b5308251e9d4682e6de6037c76bccb43bf9050c1
panawara <pa.nawara@gmail.com>
2017-07-10 23:46:14 +0200

    end of exercise3, uppercase, text etc.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period."
+var dinosaur = "triceratops"
 
-console.log(allNames);
+var nameUpperCased = dinosaur.toUpperCase();
+console.log(nameUpperCased);
+
+var textCharsAfter = text.replace("Velociraptor", nameUpperCased);
+console.log(textCharsAfter);
+
+var partOftext = textCharsAfter.slice(0,72)
+console.log(partOftext);
