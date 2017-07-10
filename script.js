5a42625ece1bde128314cea10a55e58a95fb7c64
panawara <pa.nawara@gmail.com>
2017-07-10 21:51:39 +0200

    solve of exercise1, trianglearea

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function getTriangleArea(a, h) {
+	if (a>0 && h>0) {
+		return (a*h/2)
+	} else if (a<0 && h<0) {
+		return "Nieprawidłowe dane"
+	}
+}
+
+console.log(getTriangleArea(10,6));
+
+
+var triangleArea = getTriangleArea(10,15);
+console.log(triangleArea);
