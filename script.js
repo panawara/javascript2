e4368b2677429925ec0295ebe2d31361878f0af5
panawara <pa.nawara@gmail.com>
2017-07-16 13:36:19 +0200

    corrected exercise1

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-
 function getTriangleArea(a, h) {
-	if (a>0 && h>0) {
-		return (a*h/2)
-	} else if (a<0 && h<0) {
-		return "Nieprawidłowe dane"
+	if (a > 0 && h > 0) {
+		return (a * h / 2);
+	}				{
+		return "Nieprawidłowe dane";
+	}
 	}
 }
 
-console.log(getTriangleArea(10,6));
+console.log(getTriangleArea(10, 6));
 
 
-var triangleArea = getTriangleArea(10,15);
+var triangleArea = getTriangleArea(10, 15);
 console.log(triangleArea);
