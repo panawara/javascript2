f900a4fd1757fb663e6323102ff34fea664391c3
panawara <pa.nawara@gmail.com>
2017-07-17 23:48:52 +0200

    exercise4, drawtree

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period."
-var dinosaur = "triceratops"
-
-var nameUpperCased = dinosaur.toUpperCase();
-console.log(nameUpperCased);
-
-var textCharsAfter = text.replace("Velociraptor", nameUpperCased);
-console.log(textCharsAfter);
-
-var partOftext = textCharsAfter.slice(0,72)
-console.log(partOftext);
+function star() {
+	return "*";
+}
+function drawTree(n) {
+	for( i = 1 ; i <= n ; i ++ ) {
+		for( j = n - 1 ; j >= i ; j-- ); 
+			document.write("&nbsp;");
+		for( j = 1 ; j <= ( 2 * i - 1 ) ; j++ ) 
+			document.write(star());
+			document.write("<br/>");
+	}
+}
+verse = parseInt(prompt("Podaj ilość wierszy:"));
+drawTree(verse);
