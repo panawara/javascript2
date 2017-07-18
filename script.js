bcf10a666d930b5af89ab13e423fde29e42dc028
panawara <pa.nawara@gmail.com>
2017-07-18 08:49:00 +0200

    not finished

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-function star() {
-	return "*";
-}
-function drawTree(n) {
-	for( i = 1 ; i <= n ; i ++ ) {
-		for( j = n - 1 ; j >= i ; j-- ); 
-			document.write("&nbsp;");
-		for( j = 1 ; j <= ( 2 * i - 1 ) ; j++ ) 
-			document.write(star());
-			document.write("<br/>");
-	}
-}
-verse = parseInt(prompt("Podaj ilość wierszy:"));
-drawTree(verse);
+var ItemsClass = document.getElementsByClassName('button');
+
+console.log(ItemsClass);
+
+var name = ItemsClass.lenght
+console.log(name)
