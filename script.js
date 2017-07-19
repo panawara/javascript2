798c1b25af76a14a474ed9c74269a9956968163a
panawara <pa.nawara@gmail.com>
2017-07-19 09:34:33 +0200

    why? if -> console.log(allNames)

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
-
 console.log(allNames);
 
-var newName = 'Marian'
+var newName = 'Marian';
 
 if (allNames.indexOf(newName) === -1) {
-	var addName = allNames.concat(newName)
-	console.log(addName)
+	var addName = allNames.push(newName);
+	console.log(allNames);
+	console.log(addName);
 } else {
-	console.log(false)
+	console.log(false);
 }
