9fde64ee5634c88aa64000a282ab9032f7bc2df1
panawara <pa.nawara@gmail.com>
2017-07-25 21:49:40 +0200

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function drawTree(n) {
 
-for ( i = 1; i <= n ; i ++ ) {
-	star();
-{
-    for ( j = 1 ; j <= i ; j ++ )
-    {
-    star(star += '*');
+  for ( i = 1; i <= n ; i ++ ) {
+    var star = '';
+
+    for ( j = 1 ; j <= i ; j ++ ) {
+      star = star + '*';
+    }
+    
+    console.log(star);
+  }
 }
-}
-}
-console.log(star)};
+
+
+drawTree(15);
