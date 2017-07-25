e7eebdac9af9baee207467f3db2986c6525e8f15
panawara <pa.nawara@gmail.com>
2017-07-25 23:09:13 +0200

    part of exercise8

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-var list = document.getElementById('list')
+var newGameBtn = document.getElementById('js-newGameButton');
 
-add.addEventListener('click', function() {});
+newGameBtn.addEventListener('click', newGame);
 
-var element = document.createElement('li');
+var pickRock = document.getElementById('js-playerPick_rock'),
+	pickPaper = document.getElementById('js-playerPick_paper'),
+	pickScissors = document.getElementById('js-playerPick_scissors');
 
-list.appendChild(element);
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
+
+var gameState = 'notStarted', 
+    player = {
+        name: '',
+        score: 0
+    },
+    computer = {
+        score: 0
+    };
+
+var newGameElem = document.getElementById('js-newGameElement'),
+	pickElem = document.getElementById('js-playerPickElement'),
+	resultsElem = document.getElementById('js-resultsTableElement');
+
+function setGameElements() {
+  	switch(gameState) {
+    	case 'started':
+        	newGameElem.style.display = 'none';
+        	pickElem.style.display = 'block';
+        	resultsElem.style.display = 'block';
+      	break;
+    	case 'ended':
+        	newGameBtn.innerText = 'Jeszcze raz';
+    	case 'notStarted':
+    	default:
+	        newGameElem.style.display = 'block';
+	        pickElem.style.display = 'none';
+	        resultsElem.style.display = 'none';
+  }
+}
